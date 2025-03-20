b2a46b15ec6e1e2de42f2fb7f20f064dead14fd4
Robin van Baalen <robin@ganttify.com>
2025-03-20 19:59:21 -0400

    feat: add manual build and deploy workflow with readme-to-html configuration

---
+++ rtoh.config.js
@@ -0,0 +1,19 @@
+/**
+ * Configuration file for readme-to-html
+ * Generated with the --init command
+ */
+export default {
+  // Use a remote template (no need to create your own)
+  templatePath: "https://raw.githubusercontent.com/rvanbaalen/rvanbaalen.github.io/refs/heads/main/templates/project-template.html",
+  
+  // Custom stylesheet (optional)
+  stylesheetPath: "https://raw.githubusercontent.com/rvanbaalen/rvanbaalen.github.io/refs/heads/main/templates/style.css",
+  
+  // Uncomment and customize any additional options as needed:
+  // outputPath: "index.html",
+  // readmePath: "README.md",
+  // excludeFromNav: ["description", "contributing", "license"],
+  // replace: {
+  //   "/css/style.css": "./src/custom.css"
+  // }
+}
